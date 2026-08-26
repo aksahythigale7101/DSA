dcf2067d8a12a93992699e3b874bcb98e538687c
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-26 10:55:56 +0530

    Base Check Script Add

---
+++ BaseCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DSA
+{
+    internal class BaseCheck
+    {
+
+        public void Show()
+        {
+            Console.WriteLine("BaseCheck Show method called.");
+        }
+
+
+
+    }
+}
