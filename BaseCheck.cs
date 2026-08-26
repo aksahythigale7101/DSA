e20686fe8f6d6651e82b2c87289d0d668003b803
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-26 17:34:37 +0530

    comment

--- BaseCheck.cs
+++ BaseCheck.cs
@@ -77,9 +77,12 @@ namespace DSA
         public void Headcheck2()// revret
         {
             Console.WriteLine("This is Bat car");
+
             Console.WriteLine("This is mma fighter");
             Console.WriteLine("This is fotball car");
           
+
+
         }
 
 
